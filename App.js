961aec7590440e77e409cca65e640567a13e6045
Mickael Lecoq <mickael@lecoq.me>
2021-09-06 11:30:58 +0200

    init project

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  BottomSheetModalProvider,
+  BottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import React, { useMemo, useRef } from "react";
+import { StyleSheet, View, Button } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("MAPBOX_ACCESS_TOKEN");
 
 export default function App() {
+  const snapPoints = useMemo(() => ["90%"], []);
+
+  const bottomSheet = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <Button
+          title="open bottom sheet"
+          onPress={() => bottomSheet.current?.present()}
+        />
+      </View>
+      <BottomSheetModal
+        name="example"
+        ref={bottomSheet}
+        snapPoints={snapPoints}
+      >
+        <View flex={1}>
+          <MapboxGL.MapView compassViewPosition={3} style={styles.mapView}>
+            <MapboxGL.Camera zoomLevel={3} />
+          </MapboxGL.MapView>
+        </View>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  mapView: { flex: 1 },
 });
